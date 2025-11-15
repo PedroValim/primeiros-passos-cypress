4fa09ac8d199ebdd260ceb1965de0ccf36d47062
PedroValim <pedrohlvalim@gmail.com>
2025-11-15 12:37:26 -0300

    Add page objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,29 +1,33 @@
 import userData from '../fixtures/users/userData.json'
+import LoginPage from '../pages/loginPAge'
+import DashboardPage from '../pages/dashboardPage'
+
+const loginPage = new loginPage()
 
 describe('Orange HRM Tests', () => {
 
   const selectorsList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
-    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    
+   
     dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']",
+   
     myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
     firstNameField: "[name='firstName']",
     lastNameField: "[name='lastName']",
     genericField: ".oxd-input--active",
     dateField: "[placeholder='yyy-mm-dd']",
+    genericCombobox: ".oxd-select-text--arrow",
+    secondItemCombobox: ".oxd-select-dropdown > :nth-child(2)",
+    thirdItemCombobox: ".oxd-select-dropdown > :nth-child(3)",
     dateCloseButton: ".--close",
+    submitButton: ".orangehrm-left-space"
   }
 
   it.only('User Info Update - Success', () => {
-
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type('Admin');
-    cy.get(selectorsList.passwordField).type('admin123');
-    cy.get(selectorsList.loginButton).click();
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index');
+    loginPage.accessLoginPage()
+    loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
+    DashboardPage.checkDashboardPage()
+    
     cy.get(selectorsList.dashboardGrid)
     cy.get(selectorsList.myInfoButton).click()
     cy.get(selectorsList.firstNameField).clear().type('FirstNameTeste')
@@ -38,6 +42,12 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorsList.genericField).eq(9).clear().type('sinNumberTest')
     cy.get(selectorsList.genericField).eq(0).click()
     cy.get('.oxd-toat-close')
+
+    cy.get(selectorsList.genericCombobox).eq(0).click({force:true})
+    cy.get(selectorsList.secondItemCombobox).click()
+    cy.get(selectorsList.genericCombobox).eq(1).click({force:true})
+    cy.get(selectorsList.thirdItemCombobox).click()
+
 
   })
   it('Login - Fail', () => {
